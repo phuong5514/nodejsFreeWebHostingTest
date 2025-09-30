8e25407d9c799f1790f4a6630de0f504fca3b153
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-09-30 15:00:31 +0700

    fix some bugs

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 let http = require('http');
 let fs = require('fs');
 let path = require('path');
+let url = require('url');
 
 const PORT = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
-    if (req.url === '/' || req.url === '/index.html') {
+    // Parse URL to get pathname without query parameters
+    const parsedUrl = url.parse(req.url);
+    const pathname = parsedUrl.pathname;
+    
+    if (pathname === '/' || pathname === '/index.html') {
         const filePath = path.join(__dirname, 'frontend/index.html');
         
         fs.readFile(filePath, function(error, content) {
