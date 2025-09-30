31676f433c5c4460b29e621fc7980aa395da26b6
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-09-30 17:22:24 +0700

    a

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
-let http = require('http');
-let fs = require('fs');
-let path = require('path');
-let url = require('url');
+// let http = require('http');
+// let fs = require('fs');
+// let path = require('path');
+// let url = require('url');
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-http.createServer(function (req, res) {
-    // Parse URL to get pathname without query parameters
-    const parsedUrl = url.parse(req.url);
-    const pathname = parsedUrl.pathname;
+// http.createServer(function (req, res) {
+//     // Parse URL to get pathname without query parameters
+//     const parsedUrl = url.parse(req.url);
+//     const pathname = parsedUrl.pathname;
     
-    if (pathname === '/' || pathname === '/index.html') {
-        const filePath = path.join(__dirname, 'frontend/index.html');
+//     if (pathname === '/' || pathname === '/index.html') {
+//         const filePath = path.join(__dirname, 'frontend/index.html');
         
-        fs.readFile(filePath, function(error, content) {
-            if (error) {
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                res.end(`Error: ${error.code}`);
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(content);
-            }
-        });
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('404 Not Found');
-    }
-}).listen(PORT);
+//         fs.readFile(filePath, function(error, content) {
+//             if (error) {
+//                 res.writeHead(500, {'Content-Type': 'text/html'});
+//                 res.end(`Error: ${error.code}`);
+//             } else {
+//                 res.writeHead(200, {'Content-Type': 'text/html'});
+//                 res.end(content);
+//             }
+//         });
+//     } else {
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         res.end('404 Not Found');
+//     }
+// }).listen(PORT);
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express on " + PORT);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 console.log(`Server running on port ${PORT}`);
